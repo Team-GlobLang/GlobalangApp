05d5a4f9f22898066bb89cf76d36875351dbf7ef
Akion <akion.cheng.jimenez@est.una.ac.cr>
2025-10-28 08:40:17 -0600

    REF: REFACTOR DE RANKING SEPARADO EN PAGE-CONTAINER-CARD

---
+++ src/feautures/Ranking/Components/CardLocalContainerRanking.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="p-5 bg-white shadow-lg rounded-2xl w-11/12">
+    <h2 class="text-2xl font-bold mb-4 text-center">
+      🏆 Local Top - {{ country }}
+    </h2>
+
+    <div v-if="isLoading" class="space-y-2 animate-pulse">
+      <div v-for="n in 4" :key="n" class="h-10 bg-gray-200 rounded-xl"></div>
+    </div>
+    <div v-else>
+      <CardRanking
+        v-for="rank in ranking"
+        :country="country"
+        :userName="rank.fullName"
+        :position="rank.position"
+        :totalRecords="rank.totalRecords"
+        :totalPoints="rank.totalPoints"
+      />
+    </div>
+
+    <p class="text-center text-lg font-bold mb-4">Your current position</p>
+    <CardRanking
+      v-if="userRank"
+      :country="country"
+      :userName="userRank.fullName"
+      :position="userRank.position"
+      :totalRecords="userRank.totalRecords"
+      :totalPoints="userRank.totalPoints"
+    />
+
+    <div
+      v-if="!isLoadingUser && !userRank"
+      class="p-3 text-gray-500 text-center"
+    >
+      You dont have score yet
+    </div>
+
+    <div
+      v-if="!isLoading && ranking.length === 0"
+      class="p-3 text-gray-500 text-center"
+    >
+      No users to display
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useInfiniteQuery, useQuery } from "@tanstack/vue-query";
+import { computed } from "vue";
+import { userStore } from "@UserStore";
+import { GetRankin, GetRankinUser } from "../Service/Ranking";
+import type { Rank } from "../Types/Rank";
+import type { PaginatedResponse } from "src/feautures/shared/Interfaces/interfaces";
+import CardRanking from "./CardRanking.vue";
+const country = computed(() => userStore.user!.country);
+
+const { data, isLoading } = useInfiniteQuery<PaginatedResponse<Rank>, Error>({
+  queryKey: computed(() => ["Global_Rank", country.value]),
+  queryFn: async ({ pageParam = 1 }) => {
+    const page = pageParam as number;
+    return await GetRankin({
+      country: country.value,
+      page,
+      limit: 3,
+    });
+  },
+  initialPageParam: 1,
+  getNextPageParam: (lastPage, allPages) => {
+    return lastPage.meta?.hasNextPage ? allPages.length + 1 : undefined;
+  },
+  getPreviousPageParam: (firstPage, allPages) => {
+    return firstPage.meta?.hasPrevPage ? allPages.length - 1 : undefined;
+  },
+});
+
+const ranking = computed(
+  () => data.value?.pages.flatMap((page) => page.data).slice(0, 3) ?? []
+);
+
+const { data: userData, isLoading: isLoadingUser } = useQuery({
+  queryKey: computed(() => ["Global_User_Rank", country.value]),
+  queryFn: () => GetRankinUser({ country: country.value }),
+});
+
+const userRank = computed(() => {
+  return userData.value?.data || null;
+});
+</script>
+
+<style scoped></style>
---
+++ src/feautures/Ranking/Components/CardRanking.vue
@@ -0,0 +1,51 @@
+<template>
+  <div
+    :key="props.userName"
+    class="w-full flex justify-between items-center mb-2 p-3 rounded-xl shadow-sm hover:shadow-md transition-shadow bg-gray-50"
+  >
+    <div class="flex items-center gap-3">
+      <span class="text-lg">
+        {{ props.position > 3 ? props.position : getMedal(props.position) }}
+      </span>
+      <span class="font-medium">{{ props.userName }}</span>
+    </div>
+    <div class="text-right">
+      <div class="text-sm text-gray-500">{{ props.totalRecords }} quizzes</div>
+      <div class="font-bold text-blue-600">{{ props.totalPoints }} pts</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  country: {
+    type: String,
+  },
+  userName: {
+    type: String,
+  },
+  position: {
+    type: Number,
+    required: true,
+  },
+  totalRecords: {
+    type: Number,
+  },
+  totalPoints: {
+    type: Number,
+  },
+});
+
+function getMedal(index: number): string {
+  switch (index) {
+    case 1:
+      return "🥇";
+    case 2:
+      return "🥈";
+    case 3:
+      return "🥉";
+    default:
+      return "";
+  }
+}
+</script>
